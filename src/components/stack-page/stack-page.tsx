578b654088be40e81eb4d9d3ec8aaa4dd118e1f1
alsplatonov <platon1560@gmail.com>
2023-08-05 17:16:36 +0300

    finished stack page

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,132 @@
-import React from "react";
+import { FC, ChangeEvent, FormEvent, useState } from "react";
+import styles from "./stack.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Button } from "../ui/button/button";
+import { ElementStates } from "../../types/element-states";
+import { Input } from "../ui/input/input";
+import { Circle } from "../ui/circle/circle";
+import { Stack } from "./stack";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
+import { setDelay } from "../../utils/utils";
 
-export const StackPage: React.FC = () => {
+interface IStackState<T> {
+  item: T;
+  state: ElementStates;
+}
+
+export const StackPage: FC = () => {
+  const [value, setValue] = useState("");
+  const [inProgress, setInProgress] = useState(false);
+  const [stack, setStack] = useState<Stack<string>>(new Stack<string>());
+  const [stackState, setStackState] = useState<IStackState<string>[]>([]);
+
+
+  const addItem = async (e: FormEvent, item: string) => {
+    e.preventDefault();
+    if (!value || stackState.length > 19 || inProgress) return;
+    setInProgress(true);
+
+    // Добавить элемент в стек
+    stack.push(value);
+
+    // Создать новый элемент с state = "changing" и добавить в stackState
+    const newItem: IStackState<string> = { item: value, state: ElementStates.Changing };
+    setStackState((prevState) => [...prevState, newItem]);
+
+    await setDelay(SHORT_DELAY_IN_MS); // Используем функцию setDelay
+
+    const index = stackState.length - 1;
+
+    // Обновить состояние только что добавленного элемента до значения Default
+    const lastAddedItem = stack.peek();
+    if (lastAddedItem) {
+      setStackState((prevState) => {
+        const updatedIndex = prevState.findIndex(item => item.item === lastAddedItem);
+        if (updatedIndex !== -1) {
+          const updatedItem = { ...prevState[updatedIndex], state: ElementStates.Default };
+          const updatedState = [...prevState];
+          updatedState[updatedIndex] = updatedItem;
+          return updatedState;
+        }
+        return prevState;
+      });
+    }
+
+    setInProgress(false);
+    setValue("");
+  };
+
+
+  const deleteItem = async () => {
+    const lastAddedItem = stack.peek();
+
+    if (lastAddedItem) {
+      const lastAddedIndex = stackState.findIndex(item => item.item === lastAddedItem);
+
+      if (lastAddedIndex !== -1) {
+        setStackState(prevState => {
+          const updatedItem = { ...prevState[lastAddedIndex], state: ElementStates.Changing };
+          const updatedState = [...prevState];
+          updatedState[lastAddedIndex] = updatedItem;
+          return updatedState;
+        });
+
+        await setDelay(SHORT_DELAY_IN_MS);
+
+        stack.pop();
+
+        setStackState(prevState => prevState.slice(0, -1));
+      }
+    }
+  };
+
+
   return (
     <SolutionLayout title="Стек">
-
+      <section className={styles.main}>
+        <form className={styles.form} onSubmit={(e) => addItem(e, value)}>
+          <Input
+            isLimitText={true}
+            maxLength={4}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
+            value={value}
+          />
+          <Button
+            disabled={value.length <= 0 || stackState.length > 19 || inProgress}
+            onClick={(e) => {
+              addItem(e, value);
+            }}
+            isLoader={false}
+            text={"Добавить"}
+          />
+          <Button
+            disabled={stack.getSize() <= 0 || inProgress}
+            onClick={deleteItem}
+            isLoader={false}
+            text={"Удалить"}
+          />
+          <Button
+            disabled={stack.getSize() <= 0 || inProgress}
+            text={"Очистить"}
+            onClick={() => {
+              stack.clear();
+              setStackState([]);
+            }}
+          />
+        </form>
+        <ul className={styles.stack}>
+          {stackState.map((item, index) => (
+            <li key={index} className={styles.item}>
+              <Circle
+                state={item.state}
+                index={index}
+                letter={item.item}
+                head={stackState.length - 1 === index ? "top" : ""}
+              />
+            </li>
+          ))}
+        </ul>
+      </section>
     </SolutionLayout>
   );
 };
---
+++ src/components/stack-page/stack.ts
@@ -0,0 +1,44 @@
+import { ElementStates } from "../../types/element-states";
+
+export interface IStackState<T> {
+  item: T;
+  state: ElementStates;
+}
+
+export class Stack<T> {
+  private items: IStackState<T>[]; 
+
+  constructor() {
+    this.items = [];
+  }
+
+  // Добавление элемента в стек
+  push(item: T): void {
+    this.items.push({ item, state: ElementStates.Default }); 
+  }
+
+  // Удаление и возврат последнего элемента из стека
+  pop(): T | undefined {
+    return this.items.pop()?.item;
+  }
+
+  // Возврат последнего элемента из стека без его удаления
+  peek(): T | undefined {
+    return this.items[this.items.length - 1]?.item;
+  }
+
+  // Проверка, пуст ли стек
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
+
+  // Возврат размера стека
+  getSize(): number {
+    return this.items.length;
+  }
+
+  // Очистка стека
+  clear(): void {
+    this.items = [];
+  }
+}
