094ecd83f63851ebc8c6fa3db3e9a0699ca8b364
alsplatonov <platon1560@gmail.com>
2023-09-23 01:40:03 +0300

    dependencies fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,25 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import "./index.css";
+// import "./fonts/fonts.css";
+// import "./components/ui/common.css";
+// import "./components/ui/box.css";
+// import App from "./components/app/app";
+// import reportWebVitals from "./reportWebVitals";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+// reportWebVitals();
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./index.css";
 import "./fonts/fonts.css";
 import "./components/ui/common.css";
 import "./components/ui/box.css";
@@ -10,11 +28,12 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
