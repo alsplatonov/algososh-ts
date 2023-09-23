75adf713878687931736ab04e5e837a06ae9bebe
alsplatonov <platon1560@gmail.com>
2023-09-23 11:41:25 +0300

    add const.ts

---
+++ cypress/constants.ts
@@ -0,0 +1,18 @@
+export const testUrl = "http://localhost:3000";
+
+
+export const testInput = '[data-testid=input]';
+export const testCircle = '[data-testid=circle]';
+//list
+export const testMainInput = '[data-testid=main-input]';
+export const testIndexInput = '[data-testid=index-input]';
+export const testListItem = '[data-testid=listItem]';
+export const testCircleHead = '[data-testid=circle-head]';
+export const testCircleTail = '[data-testid=circle-tail]';
+export const testCircleSmall = '[data-testid=circleUpSmall]';
+//queue + stack + fib
+export const testAddBtn = '[data-testid=add-button]';
+
+//string
+export const testStringReverseBtn = '[data-testid=reverse-button]';
+
